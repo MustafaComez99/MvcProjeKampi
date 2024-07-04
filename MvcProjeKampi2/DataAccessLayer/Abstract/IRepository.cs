01587937a7a176f5ee5378708809ae6509a35c09
MustafaComez <m.comez3437@gmail.com>
2024-07-05 00:35:09 +0300

    MvcProjeKampı
    Murat Yücedağ - Mvc Proje Kampı Görev(1/4)

---
+++ MvcProjeKampi2/DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>//t type demek. Hangi entity gönderilirse.
+    {
+        //CRUD operasyonlarını birer metod olarak tanımlayacağız.
+        //Listeleme için bir metod tanımlayacağız.
+        List<T> List();
+
+        //Ekleme
+        void Insert(T p);
+
+        //Güncelleme
+        void Update(T p);
+
+        //Silme
+        void Delete(T p);
+
+        //Şartlı Listeleme
+         List<T> List(Expression<Func<T, bool>> filter);
+    }
+}
